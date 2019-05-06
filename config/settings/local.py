02f4de44c8f912aac5b229a1349915fc19937779
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-05-06 23:18:36 +0800

    initial commit

---
+++ config/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *  # noqa
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = 'localhost'
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = 1025
