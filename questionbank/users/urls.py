2ac6f705a8bbbc4ffcb6036111ea7cf62a1cf0f3
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-05-08 22:55:32 +0800

    user CRUD

--- questionbank/users/urls.py
+++ questionbank/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserListView, UserUpdateView
+from .views import UserListView, UserUpdateView, UserProfileView
 
 
 app_name = 'users'
 urlpatterns = [
     path('', UserListView.as_view(), name='list'),
-    path('<int:pk>/', UserUpdateView.as_view(), name='update')
+    path('<int:pk>/', UserUpdateView.as_view(), name='update'),
+    path('profile/', UserProfileView.as_view(), name='profile')
 ]
--- questionbank/users/views.py
+++ questionbank/users/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, UpdateView
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
 from django.contrib.auth import get_user_model
 
@@ -15,3 +15,14 @@ class UserListView(PermissionRequiredMixin, ListView):
 class UserUpdateView(PermissionRequiredMixin, UpdateView):
     permission_required = 'users.change_user'
     model = User
+    fields = ('username', 'email')
+
+
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    fields = ('username', 'email')
+    template_name = 'users/user_profile.html'
+
+    def get_object(self):
+        obj = User.objects.get(pk=self.request.user.pk)
+        return obj
