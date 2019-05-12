1de4c8a3d5cc9b3e60c62cee87decac1d0b2d589
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-05-12 21:42:43 +0800

    test user views

--- questionbank/users/tests/test_views.py
+++ questionbank/users/tests/test_views.py
@@ -1,6 +1,7 @@
 import pytest
+from django.shortcuts import Http404
 
-from ..views import UserProfileView
+from ..views import UserProfileView, AcceptInvitationView
 
 pytestmark = pytest.mark.django_db
 
@@ -18,3 +19,53 @@ def test_user_profile_get_obj(user, rf):
 
     obj = view.get_object()
     assert obj == user
+
+
+def test_accept_invite_view_get_token(invite, user, rf):
+    """
+    AcceptInvitationView.get_token() should return a token
+    """
+    request = rf.get('/test/')
+    request.user = user
+
+    view = AcceptInvitationView(kwargs={'token': invite.token})
+    view.request = request
+
+    token = view.get_token()
+
+    assert token == invite.token
+
+
+def test_accept_invite_view_dispatch(user, rf):
+    """
+    AcceptInvitationView.dispatch() should raise 404 if the user
+    is authenticated
+    """
+    request = rf.get('/test/')
+    request.user = user
+
+    view = AcceptInvitationView()
+
+    with pytest.raises(Http404):
+        view.dispatch(request)
+
+
+def test_accept_invite_view_get_initial(user, rf):
+    """
+    AcceptInvitationView.dispatch() should raise 404 if the user
+    is authenticated
+    """
+    request = rf.get('/test/')
+    request.user = user
+
+    view = AcceptInvitationView(
+        invite_instance={
+            'username': 'test_username',
+            'email': 'test@email.com'
+        }
+    )
+
+    initial = view.get_initial()
+
+    assert initial['username'] == 'test_username'
+    assert initial['email'] == 'test@email.com'
