6f94f7acc5e1d978b7b071d0ef9960a7ebbaff10
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-05-13 21:24:44 +0800

    fix failing test

--- questionbank/invites/tests/factories.py
+++ questionbank/invites/tests/factories.py
@@ -1,12 +1,14 @@
-from factory import DjangoModelFactory, Faker
+from factory import DjangoModelFactory, Faker, SubFactory
 
 from questionbank.invites.models import Invite
+from questionbank.users.tests.factories import UserFactory
 
 
 class InviteFactory(DjangoModelFactory):
     username = Faker('user_name')
     email = Faker('email')
     role = 'Admin'
+    created_by = SubFactory(UserFactory)
 
     class Meta:
         model = Invite
--- questionbank/invites/tests/test_models.py
+++ questionbank/invites/tests/test_models.py
@@ -6,14 +6,15 @@ from questionbank.invites.models import Invite
 pytestmark = pytest.mark.django_db
 
 
-def test_invite_model_save():
+def test_invite_model_save(user):
     """
     calling Invite.save() should create a random token
     """
     invite = Invite.objects.create(
         username='test',
         email='test',
-        role=ADMIN
+        role=ADMIN,
+        created_by=user
     )
     # the token should exist
     assert invite.token
