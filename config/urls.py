2c3e5ca608e06e23b6a98eff596bbfcd5a9b7f43
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-05-20 06:32:07 +0800

    add error pages

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views import defaults as default_views
 
 urlpatterns = [
     path('', include('questionbank.dashboards.urls')),
@@ -20,3 +21,19 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls))
     ] + urlpatterns
+
+    urlpatterns += [
+        path(
+            '400/', default_views.bad_request,
+            kwargs={'exception': Exception('Bad Request!')},
+        ),
+        path(
+            '403/', default_views.permission_denied,
+            kwargs={'exception': Exception('Permission Denied')},
+        ),
+        path(
+            '404/', default_views.page_not_found,
+            kwargs={'exception': Exception('Page not Found')},
+        ),
+        path('500/', default_views.server_error),
+    ]
