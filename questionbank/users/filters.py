385624a72d3c5c585640a83d5dc8b61f36499000
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-05-21 14:57:50 +0800

    filter user by specialty

--- questionbank/users/filters.py
+++ questionbank/users/filters.py
@@ -2,10 +2,15 @@ import django_filters
 
 from django.contrib.auth import get_user_model
 
+from .models import Specialty
+
 User = get_user_model()
 
 
 class UserFilter(django_filters.FilterSet):
+    specialty = django_filters.ModelChoiceFilter(
+        queryset=Specialty.objects.all().order_by('name')
+    )
 
     class Meta:
         model = User
