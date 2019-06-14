a519d556a637e1fa3ecc2eb14bdef6579076c3b7
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-14 20:29:17 +0800

    add test

--- questionbank/questions/tests/test_forms.py
+++ questionbank/questions/tests/test_forms.py
@@ -1,4 +1,9 @@
+import pytest
+from django.forms import model_to_dict
+
 from questionbank.questions.forms import QuestionForm
+
+pytestmark = pytest.mark.django_db
 
 
 def test_question_form():
@@ -7,3 +12,23 @@ def test_question_form():
     """
     form = QuestionForm()
     assert not form.fields['tags'].required
+
+
+def test_question_form_exam_field(question, exam):
+    """
+    QuestionForm should be able to save add exam instance on question
+    """
+    # question should not have any exam yet
+    assert not question.exam_set.first()
+
+    data = model_to_dict(question)
+    data['exam'] = exam.pk
+    form = QuestionForm(
+        data=data
+    )
+
+    assert form.is_valid()
+    instance = form.save()
+
+    # question should have exam instance
+    assert instance.exam_set.first().pk
