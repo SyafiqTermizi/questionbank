d7e4c66034f7ffaca977560ea6c2e8e6c5f20082
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-23 23:03:58 +0800

    update failing test

--- questionbank/comments/tests/test_mixins.py
+++ questionbank/comments/tests/test_mixins.py
@@ -3,13 +3,15 @@ from django.shortcuts import reverse
 from questionbank.comments.mixins import QuestionSuccessUrlMixin, ExamSuccessUrlMixin
 
 
-def test_question_success_url_mixin():
+def test_question_success_url_mixin(rf):
     """
     QuestionSuccessUrlMixin.get_success_url() should return url to
     question list with unresolved query param.
     /comments/questions/1/?is_resolved=false
     """
     mixin = QuestionSuccessUrlMixin()
+    request = rf.get('/test/')
+    mixin.request = request
     mixin.kwargs = {'question_id': 1}
     url = mixin.get_success_url()
 
