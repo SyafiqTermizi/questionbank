7399ac39c4d9b4bd069cf66b63f37f2a8669dec3
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-24 22:56:07 +0800

    add subject name filter

--- questionbank/questions/filters.py
+++ questionbank/questions/filters.py
@@ -5,9 +5,15 @@ from .models import Question
 
 class QuestionFilter(django_filters.FilterSet):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters['tags__name__icontains'].label = 'Tags name'
+        self.filters['course__name__icontains'].label = 'Course name'
+
     class Meta:
         model = Question
         fields = {
             'course__code': ['iexact'],
-            'tags__name': ['icontains']
+            'tags__name': ['icontains'],
+            'course__name': ['icontains']
         }
