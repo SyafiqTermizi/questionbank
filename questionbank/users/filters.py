e79f37588d420b408bb7ef83d9120b7fc10b3fff
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-26 17:43:35 +0800

    filter user by role

--- questionbank/users/filters.py
+++ questionbank/users/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 from questionbank.users.models import Specialty
 
@@ -9,8 +10,17 @@ User = get_user_model()
 
 class UserFilter(django_filters.FilterSet):
     specialty = django_filters.ModelChoiceFilter(
-        queryset=Specialty.objects.all().order_by('name')
+        queryset=Specialty.objects.order_by('name')
     )
+    groups = django_filters.ModelChoiceFilter(
+        queryset=Group.objects.order_by('name'),
+        label='Role'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filters['username__icontains'].label = 'Username'
+        self.filters['email__icontains'].label = 'Email'
 
     class Meta:
         model = User
