0ae7e6abc1e9a7ab23801ed73aa48a3c2cfaa049
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-27 18:28:45 +0800

    update question list mixin

--- questionbank/questions/mixins.py
+++ questionbank/questions/mixins.py
@@ -1,5 +1,7 @@
 from django.shortcuts import reverse
 
+from questionbank.users.constants import COORDINATOR
+
 
 class LimitedQuestionMixin:
     """
@@ -7,7 +9,13 @@ class LimitedQuestionMixin:
     """
     def get_queryset(self):
         if self.request.user.is_superuser:
-            return self.model.objects.order_by('-created_at').prefetch_related('created_by', 'tags')
+            return self.model.objects\
+                .order_by('-created_at')\
+                .prefetch_related('created_by', 'tags')
+        elif self.request.user.role == COORDINATOR:
+            return self.model.objects.filter(
+                course=self.request.user.course
+            )
         return self.model.objects\
             .filter(created_by=self.request.user)\
             .order_by('-created_at').prefetch_related('created_by', 'tags')
