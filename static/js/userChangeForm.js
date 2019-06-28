4250470c8dc322aa9c203953f01c66edcc3e2cfc
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-28 22:22:38 +0800

    fix the JS issue

--- static/js/userChangeForm.js
+++ static/js/userChangeForm.js
@@ -1,13 +1,13 @@
 import $ from 'jquery'
 
 $( document ).ready(function() {
-  if ($('#id_groups_3').is(':checked')) {
+  if ($('input[type=checkbox][value=2]').is(':checked')) {
     $('#div_id_course').show()
   } else {
     $('#div_id_course').hide()
   }
 
-  $('#id_groups_3').change(function(){
+  $('input[type=checkbox][value=2]').change(function(){
     if (!this.checked) {
       $('#div_id_course').hide()
     } else {
