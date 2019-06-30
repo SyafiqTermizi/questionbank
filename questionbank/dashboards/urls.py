0af77e2a3db71a8822f64814da1fb52f3b7cde6d
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-06-30 17:21:36 +0800

    remove cache from dashboard

--- questionbank/dashboards/urls.py
+++ questionbank/dashboards/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from .views import Dashboard
 
 
 app_name = 'dashboards'
 urlpatterns = [
-    path('', cache_page(60*30)(Dashboard.as_view()), name='dashboard')
+    path('', Dashboard.as_view(), name='dashboard')
 ]
