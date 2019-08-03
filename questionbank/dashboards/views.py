1bcf2cfa9212dc3d5c9a2810a66139c037c69033
SyafiqTermizi <ahmadsyafiq93@gmail.com>
2019-08-03 17:36:46 +0800

    add data to dashboard

--- questionbank/dashboards/views.py
+++ questionbank/dashboards/views.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from questionbank.users.constants import ADMIN, COORDINATOR, LECTURER
+from questionbank.exams.models import Exam
+from questionbank.questions.models import Question
+from questionbank.subjects.models import Subject
+from questionbank.invites.models import Invite
+
 
 class Dashboard(LoginRequiredMixin, TemplateView):
     """
@@ -31,3 +37,33 @@ class Dashboard(LoginRequiredMixin, TemplateView):
       - specialty
     """
     template_name = 'dashboards/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # https://docs.djangoproject.com/en/2.1/ref/models/querysets/#when-querysets-are-evaluated
+        role = self.request.user.role
+
+        if role == ADMIN:
+            return self.get_admin_context(context)
+        elif role == COORDINATOR:
+            return self.get_coordinator_context(
+                context,
+                course_id=self.request.user.course.pk,
+                user_id=self.request.user.pk
+            )
+
+    def get_admin_context(self, context={}):
+        context['total_exam'] = Exam.objects.count()
+        context['total_question'] = Question.objects.count()
+        context['total_subject'] = Subject.objects.count()
+        context['total_invites'] = Invite.objects.count()
+        context['role'] = ADMIN
+        return context
+
+    def get_coordinator_context(self, context={}, course_id=0, user_id=0):
+        context['total_exam'] = Exam.objects.filter(course__id=course_id).count()
+        context['total_question'] = Question.objects.filter(course__id=course_id).count()
+        context['total_question_created'] = Question.objects.filter(created_by__id=user_id).count()
+        context['role'] = COORDINATOR
+        return context
