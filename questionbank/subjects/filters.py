52cab443b52c59248aaa45d564c37b90355be38b
syafiq <syafiq.termizi@omesti.com>
2020-10-11 01:56:33 +0800

    update subject filter

--- questionbank/subjects/filters.py
+++ questionbank/subjects/filters.py
@@ -1,13 +1,20 @@
 import django_filters
+from django.db.models import Q
 
 from .models import Subject
 
 
 class SubjectFilter(django_filters.FilterSet):
+    q = django_filters.CharFilter(
+        method='filter_code_and_name',
+        label='',
+    )
 
     class Meta:
         model = Subject
-        fields = {
-            'code': ['iexact'],
-            'name': ['icontains']
-        }
+        fields = ['q']
+
+    def filter_code_and_name(self, queryset, name, value):
+        return Subject.objects.filter(
+            Q(code__icontains=value) | Q(name__icontains=value)
+        )
